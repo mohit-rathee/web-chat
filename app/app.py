2c98eeeb6a56f59025f87995a19ad5ba06dc4d75
Parrot Live user <user@localhost.localdomain>
2022-09-11 10:24:45 +0000

    added association table

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-from socket import MsgFlag
 from flask import Flask, render_template, request, redirect, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
@@ -16,31 +15,35 @@ db = SQLAlchemy(app)
 
 # metadata=None
 
+channel_post=db.Table('channel_post',
+    db.Column('channel',db.Integer,db.ForeignKey('channel.id')),
+    db.Column('posts',db.Integer,db.ForeignKey('posts.id')))
+
 class users(db.Model):
     id=db.Column(db.Integer,primary_key=True)                            #User ID
     username=db.Column(db.String(20), unique=True, nullable=False)       #user name
     password=db.Column(db.String(30), nullable=False)                    #user password
     balance=db.Column(db.Integer, nullable=True, default=0)              #user balance
     post=db.relationship('posts',backref='user')                         #relation#
-    topic=db.relationship('topic_id',backref='user')
+    topic=db.relationship('channel',backref='user')
+
     def __init__(self, username, password, balance):
         self.username=username
         self.password=password
         self.balance=balance
 
-class topic_id(db.Model):
+class channel(db.Model):
     id=db.Column(db.Integer,primary_key=True)                   #topic ID
     name=db.Column(db.String, nullable=False,unique=True)       #topic name
-    topic=db.relationship('posts',backref='topic')              #relation with post#
     creator_id=db.Column(db.Integer,db.ForeignKey('users.id'))  #creator ID
 
 
 class posts(db.Model):
-    id=db.Column(db.Integer,primary_key=True)                   #msg/post ID
-    data=db.Column(db.String, nullable=False)                   #actuall msg
-    sender_id= db.Column(db.Integer, db.ForeignKey('users.id')) #User ID
-    topicid=db.Column(db.Integer, db.ForeignKey('topic_id.id')) #topic ID
-    time = db.Column(db.DateTime, default=func.now())           #time
+    id=db.Column(db.Integer,primary_key=True)                           #msg/post ID
+    data=db.Column(db.String, nullable=False)                           #actuall msg
+    sender_id= db.Column(db.Integer, db.ForeignKey('users.id'))         #User ID
+    time = db.Column(db.DateTime, default=func.now())                   #time
+    topic=db.relationship('channel',secondary=channel_post,backref='posts') #Association table->channel_post
 
 
 
@@ -83,8 +86,8 @@ def teardown_request(exception):
 def index():
     if session.get("id")==None:
         return redirect("/login")
-    if session.get("topic")!=None:
-        return redirect("/user/"+str(session.get("topic")))
+    if session.get("channel")!=None:
+        return redirect("/user/"+str(session.get("channel")))
     else:
         return redirect("/user")
 
@@ -147,12 +150,14 @@ def reset():
         for i in user:
             db.session.delete(i)
             db.session.commit()
-        data = topic_id.query.all()
+        data = channel.query.all()
         for i in data:
             print(i)
             db.session.delete(i)
             db.session.commit()
         print("delete")
+        session["id"]=None
+        session["channel"]=None
         return redirect("/login")
     except:
         return render_template("message.html",msg="can't delete plz check your data")
@@ -162,59 +167,65 @@ def reset():
 def user():
     if session.get("id")==None:
         return redirect("/login") 
-    session["topic"]=None   
+    session["channel"]=None   
     id=session.get("id")
+    new_channel=request.form.get("channel_name")
     user=users.query.filter_by(id=id).first()
     name=user.username
     balance=user.balance
-    topic=request.form.get("topic_name")
-    tables=topic_id.query.all()
-    if topic==None:
-        return render_template("user.html",name=name,balance=balance,tables=tables)
+    channels=channel.query.all()
+    if new_channel==None:
+        return render_template("user.html",name=name,balance=balance,tables=channels)
     try:
-        topic=topic_id(name=topic,creator_id=user.id)
+        topic=channel(name=new_channel,creator_id=user.id)
         db.session.add(topic)
         db.session.commit()
         print("sucess")
     except:
-        return render_template("message.html",msg="can't add to topic id")
+        return render_template("message.html",msg="can't add channel")
     try:
-        tables=topic_id.query.all()
-        return render_template("user.html",name=name,balance=balance,tables=tables)
+        channels=channel.query.all()
     except:
         print("can't show topics id")
+    return render_template("user.html",name=name,balance=balance,tables=channels)
 
 
 
-@app.route("/user/<topic>",methods=["GET","POST"])
-def topic_chat(topic):
+@app.route("/user/<Channel>",methods=["GET","POST"])
+def channel_chat(Channel):
     if session.get("id")==None:
         return redirect("/login")
     id=session.get("id")
     post_data=request.form.get("post")
-    Topic=str(topic)
     user = users.query.filter_by(id=id).first()
     name=user.username
     try:
-        topic=topic_id.query.filter_by(name=Topic).first()
-        session["topic"]=topic.name
-        print(session.get("topic"))
+        current_channel=channel.query.filter_by(name=Channel).first()
+        session["channel"]=current_channel.name
+        print(session.get("channel"))
     except:
-        return render_template("message.html",msg="topic not found")
-    if session.get("topic"):
+        return render_template("message.html",msg="channel not found")
+    if session.get("channel"):
         if post_data!=None:
             try:
-                post=posts(data=post_data,sender_id=user.id,topicid=topic.id)
+                post=posts(data=post_data,sender_id=user.id)
+                post.topic.append(current_channel)
                 db.session.add(post)
                 db.session.commit()
-                print("posted")
             except:
-                return render_template("message.html",msg="not added to table/topic")
-        post=posts.query.filter_by(topicid=topic.id).order_by(posts.time.asc()).all()
+                return render_template("message.html",msg="not added to channel")
+            # try:
+            #     print("added to association table")
+            # except:
+            #     return render_template('message.html',msg="can't append to association table")
+            print("posted")
+        post=current_channel.posts
+        print(post)
+        current_channel=channel.query.filter_by(name=Channel).first()
         user=users.query.order_by(users.id).all()
-        return render_template("topic_chat.html",name=name,posts=post,users=user,topic=topic)
+        return render_template("topic_chat.html",name=name,posts=current_channel.posts,users=user,topic=current_channel)
                 
-        return render_template("message.html",msg="topic don't exist")
+        return render_template("message.html",msg="channel don't exist")
     else:
         return render_template("message.html",msg="stop messing with me")
 @app.route("/shop",methods=["GET","POST"])
@@ -223,14 +234,11 @@ def shop():
         return redirect("/login")
     return render_template("shop.html")
 
-@app.route("/test/<Topic>",methods=["GET","POST"])
-def test(Topic):
-    topic=topic_id.query.filter_by(name=Topic).first()
-    name=topic.user.username
+@app.route("/test/<topic>",methods=["GET","POST"])
+def test(topic):
+    ch=channel.query.filter_by(name=topic).first()
+    name=ch.user.password
     print(name)
     return "done"
 
 
-
-def chat():
-    return "hello"
