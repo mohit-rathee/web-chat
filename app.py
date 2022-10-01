f8d25f4ecd0dab08d23074c671db009aacbc4c26
Parrot Live user <user@localhost.localdomain>
2022-10-01 20:32:13 +0000

    changed environments

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ from flask_socketio import SocketIO, join_room, emit
 app = Flask(__name__)
 app.config.from_object(__name__)
 socketio = SocketIO(app)
-app.config['SECRET_KEY'] = 'secret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://database_tc3s_user:FJlu8o0XdPhHnNF9P9jVn1yMWcIm6Hl8@dpg-ccs4njkgqg48cgsuf4vg-a.oregon-postgres.render.com/database_tc3s'
+app.config['SECRET_KEY'] = os.environ('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 db = SQLAlchemy(app)
 
 
