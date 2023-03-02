1275188eec769607f3275e3a0951a695661974ae
mohit <mohit.rathee2505@gmail.com>
2023-03-02 01:47:36 +0000

    what?

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import hashlib
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-socketio = SocketIO(app)
+# socketio = SocketIO(app)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 db = SQLAlchemy(app)
