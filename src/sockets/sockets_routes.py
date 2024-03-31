99283b07a893d59cbccc06f612a863e9e85f05f0
mohit <mohit.rathee2505@gmail.com>
2024-04-01 01:29:36 +0530

    print response time at server

--- src/sockets/sockets_routes.py
+++ src/sockets/sockets_routes.py
@@ -1,4 +1,4 @@
-import json, datetime
+import json, datetime, time
 from .. import socketio, rooms, server, tables, india_timezone
 from flask import request, session, Blueprint
 from ..database.models import media, channel, users
@@ -29,6 +29,7 @@ def on_disconnect():
 
 @socketio.on('Load')
 def Load(data):
+    start_time = time.time()
     srvr=data.get('server')
     if srvr in session.get('myserver'): # And wheater the reqsrvr is in server.keys()
         id=session.get(srvr)
@@ -67,18 +68,26 @@ def Load(data):
             peers.update({id:pub_key})
         serverInfo['live']=peers
 
+        finish_time = time.time()
+        total_time = finish_time-start_time
+        print("to create serverInfo : "+str(total_time))
         socketio.emit("server",serverInfo,to=request.sid) # send serverInfo
 
         for chnl in channels:
+            start_time = time.time()
             last_msg_id=data.get('msg').get(str(chnl.id),0)
             ch=tables[srvr][chnl.id]
             last_msgs=db.query(ch).order_by(ch.id.desc()).filter(ch.id>last_msg_id).limit(30)
             Msgs=[srvr,chnl.id]
             Msgs.append([[msg.id,msg.data,msg.user.username] for msg in last_msgs])
+            finish_time = time.time()
+            total_time = finish_time-start_time
+            print("for messages of channel : "+str(total_time))
             socketio.emit("messages",Msgs,to=request.sid)
 
 @socketio.on("create")
 def create(newchannel):
+    start_time = time.time()
     curr=newchannel[0]
     name=newchannel[1]
     id=session.get(curr)
@@ -90,6 +99,9 @@ def create(newchannel):
     if curr not in session.get("myserver"):
         return
     new = create_channel(curr,name,id)
+    finish_time = time.time()
+    total_time = finish_time-start_time
+    print("to create channel : "+str(total_time))
     socketio.emit("show_this",new,to=curr)
 
 # @socketio.on("search_text")
@@ -101,6 +113,7 @@ def create(newchannel):
 
 @socketio.on('message')
 def handel_message(message):
+    start_time = time.time()
     msg={}
     msgData=message.get('msgData')
     if msgData:
@@ -113,6 +126,7 @@ def handel_message(message):
         Media=server[curr].query(media).filter_by(id=mediaId).first()
         if Media==None:
             return
+        print('media')
         msg[1]=mediaId
     id=session.get(curr)
     name=session.get("name")
@@ -124,16 +138,21 @@ def handel_message(message):
             reply=server[curr].query(tables[curr][channel_id]).filter_by(id=int(replyId)).first()
             if reply==None:
                 return
+            print('reply')
             msg[2]=replyId
         Msg=json.dumps(msg)
         message=tables[curr][channel_id](data=Msg,sender_id=id)
         server[curr].add(message)
         server[curr].commit()
+        finish_time = time.time()
+        total_time = finish_time-start_time
+        print("to send message : "+str(total_time))
         socketio.emit('show_message',[curr,channel_id,message.id,msg,name], to=curr)
 
 # This is what E2EE looks like.
 @socketio.on('chat')
 def handel_chat(chat):
+    start_time = time.time()
     curr=chat.get('server')
     reciever=chat.get('id')
     enc_msg=chat.get('msg')
@@ -143,10 +162,14 @@ def handel_chat(chat):
     resvrEID=socketio.server.manager.rooms['/'][curr].get(reciever)
     if resvrEID:
         resvrSID=socketio.server.manager.rooms['/'][None].inverse.get(resvrEID)
+        finish_time = time.time()
+        total_time = finish_time-start_time
+        print("to send dm : "+str(total_time))
         socketio.emit('dm',[curr,id,enc_msg],to=resvrSID)
 
 @socketio.on('reaction') # Name is enough.
 def reaction(Data):
+    start_time = time.time()
     curr=Data[0]
     if curr not in session.get("myserver"):
         return
@@ -168,10 +191,14 @@ def reaction(Data):
             data=json.dumps(message)
             msg.data=data
             server[curr].commit()
+            finish_time = time.time()
+            total_time = finish_time-start_time
+            print("to react on message : "+str(total_time))
             socketio.emit('reaction',[curr,channel_id,msg.id,id,Data[3]],to=curr)
 
 @socketio.on('getHistory') # Name is enough.
 def getHistory(data):
+    start_time = time.time()
     curr=data.get("server")
     try:
         lastMsg=int(data.get('lastMsg'))
@@ -184,5 +211,8 @@ def getHistory(data):
         Msgs=[[msg.user.username,msg.id,msg.data] for msg in last_msgs]
         Msgs=[curr,channelId]
         Msgs.append([[msg.id,msg.data,msg.user.username] for msg in last_msgs])
+        finish_time = time.time()
+        total_time = finish_time-start_time
+        print("to get history : "+str(total_time))
         socketio.emit("messages",Msgs,to=request.sid)
 
